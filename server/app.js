1f3014d473c7aa363f8d505599c3ab7ee84b0348
Alexis Delgado <dalexisbr@gmail.com>
2020-11-26 22:22:46 -0700

    installing mongoose and connect to mongodb

--- server/app.js
+++ server/app.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 const schema = require('./schema/schema')
+const mongoose = require('mongoose')
+
+mongoose.connect(
+	'mongodb+srv://test:test123@cluster0.dwygs.mongodb.net/gql-test?retryWrites=true&w=majority'
+)
+mongoose.connection.once('open', () => {
+	console.log('connected to database')
+})
 
 const app = express()
 
