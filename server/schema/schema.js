ea52485ee2c8fb24bfb630521217f3a964f7982b
Alexis Delgado <dalexisbr@gmail.com>
2020-11-27 07:43:29 -0700

    Mutations

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql')
 const _ = require('lodash')
-const book = require('../models/book')
-const author = require('../models/author')
+const Book = require('../models/book')
+const Author = require('../models/author')
 
 const {
 	GraphQLObjectType,
@@ -76,6 +76,42 @@ const RootQuery = new GraphQLObjectType({
 	}
 })
 
+const Mutation = new GraphQLObjectType({
+	name: 'Mutation',
+	fields: {
+		addAuthor: {
+			type: AuthorType,
+			args: {
+				name: { type: GraphQLString },
+				age: { type: GraphQLInt }
+			},
+			resolve(parent, args) {
+				let author = new Author({
+					name: args.name,
+					age: args.age
+				})
+				return author.save()
+			}
+		},
+		addBook: {
+			type: BookType,
+			args: {
+				name: { type: GraphQLString },
+				genre: { type: GraphQLString },
+				authorId: { type: GraphQLID }
+			},
+			resolve(parent, args) {
+				return new Book({
+					name: args.name,
+					genre: args.genre,
+					authorId: args.authorId
+				}).save()
+			}
+		}
+	}
+})
+
 module.exports = new GraphQLSchema({
-	query: RootQuery
+	query: RootQuery,
+	mutation: Mutation
 })
